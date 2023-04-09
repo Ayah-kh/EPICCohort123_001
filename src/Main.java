292ad9a05cacfd51fd49d05d9f3c0d23e8dcda79
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-04-09 14:59:39 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,55 @@ public class Main {
                 + sumEvenFor);
 
         int sumEvenWhile=0;
-        int i=0;
-        while (i<=100){
-            sumEvenWhile+=i;
-            i+=2;
+        int k=0;
+        while (k<=100){
+            sumEvenWhile+=k;
+            k+=2;
         }
         System.out.println("Sum for Even numbers using while loop = "
                 + sumEvenWhile);
+
+
+        int sumEvenDoWhile=0;
+        int j=0;
+        do{
+            sumEvenDoWhile+=j;
+            j+=2;
+        }while (j<=100);
+        System.out.println("Sum for Even numbers using do-while loop = "
+                + sumEvenDoWhile);
+
+        System.out.println("=========================================");
+
+
+        //Write code to get the sum of odd numbers from 0 to 100
+        int oddEvenFor=0;
+        for (int i = 1; i <= 100; i+=2) {
+            oddEvenFor+=i;
+        }
+        System.out.println("Sum for Odd numbers using for loop = "
+                + oddEvenFor);
+
+        int oddEvenWhile=0;
+        int m=1;
+        while (m<=100){
+            oddEvenWhile+=m;
+            m+=2;
+        }
+        System.out.println("Sum for Odd numbers using while loop = "
+                + oddEvenWhile);
+
+
+        int oddEvenDoWhile=0;
+        int n=1;
+        do{
+            oddEvenDoWhile+=n;
+            n+=2;
+        }while (n<=100);
+        System.out.println("Sum for Odd numbers using do-while loop = "
+                + oddEvenDoWhile);
+
+        System.out.println("=========================================");
+
     }
 }
