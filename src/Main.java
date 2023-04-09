53a74e9c0b0eb714d2794af839a3b3361dca582e
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-04-09 14:25:19 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         //Write code to get the sum of even numbers from 0 to 100
+       int sumEvenFor=0;
+        for (int i = 0; i <= 100; i+=2) {
+         sumEvenFor+=i;
+        }
+        System.out.println("Sum for Even numbers using for loop = "
+                + sumEvenFor);
 
+        int sumEvenWhile=0;
+        while (0<=100){
+
+        }
     }
 }
